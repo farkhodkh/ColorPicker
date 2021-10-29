fa02527cf5c11083eb9118c226ae3e7cc4ff9616
farkhod <986532qw@gmail.com>
2021-10-29 12:15:53 +0300

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Farkhod on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
